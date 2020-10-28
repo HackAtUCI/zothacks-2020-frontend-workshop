32b67c87c52731647b34f6dcae57e3a230dfb4d8
Kasey Chuang <chuangky@uci.edu>
2020-10-27 17:53:26 -0700

    minor adjustments to modal fields, create scss file and moved css

--- src/app/components/createUserModal/createUserModal.js
+++ src/app/components/createUserModal/createUserModal.js
@@ -1,58 +1,55 @@
 import React, { useState, useEffect } from "react";
- 
+import './createUserModal.scss';
+
 import axios from "axios"
- 
+
 function CreateUserModal() {
- 
- 
- return (
-   <div>
 
-    <head>
-    <link rel="stylesheet" type = "text/css" href="modalstyle.css"></link>
-    </head>
 
-   <body>
-   <form class="myForm" method="get" enctype="application/x-www-form-urlencoded" action="/html/codes/html_form_handler.cfm">
- 
-   <p>
-   <label>First Name: 
-   <input type="text" name="fname" required></input>
-   </label>
-   </p>
-   <p>
-   <label>Last Name: 
-   <input type="text" name="lname" required></input>
-   </label>
-   </p>
- 
-   <p>
-     <label>Email: 
-     <input type="email" name="email_address"></input>
-     </label>
-   </p>
-  
-   <p>
-   <label>Pickup Place
-   <select id="pickup_place" name="pickup_place">
-   <option value="" selected="selected">Select One</option>
-   <option value="google" >GOOGL</option>
-   <option value="apple" >AAPL</option>
-   <option value="facebook" >FB</option>
-   </select>
-   </label>
-   </p>
- 
- 
-<p><button>Submit Account</button></p>
- 
-</form>
- 
-</body>
+  return (
+    <div>
 
-</div>
- );
+      <head>
+        <link rel="stylesheet" type="text/css" href="modalstyle.css"></link>
+      </head>
+
+      <body>
+        <form class="myForm" method="get" enctype="application/x-www-form-urlencoded" action="/html/codes/html_form_handler.cfm">
+
+          <p>
+            <label>First Name:</label>
+            <input type="text" name="fname" required></input>
+          </p>
+          <p>
+            <label>Last Name:</label>
+            <input type="text" name="lname" required></input>
+          </p>
+
+          <p>
+            <label>Email:</label>
+            <input type="email" name="email_address"></input>
+          </p>
+
+          <p>
+            <label>Favorite Stock</label>
+            <select id="fav-stock" name="fav-stock">
+                <option value="" selected="selected">Select One</option>
+                <option value="google" >GOOGL</option>
+                <option value="apple" >AAPL</option>
+                <option value="facebook" >FB</option>
+            </select>
+          </p>
+
+
+          <p><button>Create User</button></p>
+
+        </form>
+
+      </body>
+
+    </div>
+  );
 
 }
- 
+
 export default CreateUserModal;

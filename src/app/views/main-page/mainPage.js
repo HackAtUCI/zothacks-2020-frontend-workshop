ccd098460a272bd5e8c9b9e5ff68c5a2dce45a84
Kasey Chuang <chuangky@uci.edu>
2020-11-03 23:45:51 -0800

    add comments and small refactor

--- src/app/views/main-page/mainPage.js
+++ src/app/views/main-page/mainPage.js
@@ -8,45 +8,38 @@ import {CreateUserModal} from 'app/components';
 
 function MainPage() {
 
-  const [showModal, setShowModal] = useState(false);
+  // Set up state of this React component
+  const [showModal, setShowModal] = useState(false); // used to toggle modal visibility
+  const [users, setUsers] = useState(null);  // List of users from the database
+  const [stocks, setStocks] = useState(null);  // List of stocks from database
 
-  // List of users from the database
-  const [users, setUsers] = useState(null);
-
-  /// List of stocks from database
-  const [stocks, setStocks] = useState(null);
-
-  // Get the list of users from the database upon page load
+  // Get the list of users and stock from the database upon page load
   useEffect(() => {
     getUserList();
     getStockList();
   }, []);
 
-  // Get the list of users through a GET request to the backend API
+  // Get the list of users through a GET request to the backend server
   async function getUserList() {
     let users = await axios.get("https://zothacks-2020-workshop.herokuapp.com/user");
 
     // If we get a valid response, set the state object, or print an error.
-    if (users.status === 200) {
+    if (users.status === 200) { // on success
       setUsers(users.data.data);
-    } else {
+    } else { // error handling
       console.log("Error retrieving users");
     }
   }
 
-  // Get the list of stocks through a GET request to the backend API
+  // Get the list of stocks through a GET request to the backend server
   async function getStockList(){
     let stocks = await axios.get("https://zothacks-2020-workshop.herokuapp.com/stock");
-    if (stocks.status == 200){
+    if (stocks.status == 200){ // on success 
       setStocks(stocks.data.data);
     }
-    else{
+    else{ // error handling
       console.log("Error retrieving stocks");
     }
-  }
-
-  function handleCreateButtonClick(){
-    setShowModal(true);
   }
 
   function refreshPage(){
@@ -58,15 +51,11 @@ function MainPage() {
     <div>
       <div className={showModal ? "blur" : ""}>
         <UserList users={(users || [])}/>
-        <button className="button" id = "create-button" onClick={handleCreateButtonClick}>Create New User</button>
+        <button className="button" id = "create-button" onClick={() => setShowModal(true)}>Create New User</button>
       </div>
 
-    {showModal ?
-    
-      <CreateUserModal
-        stocks = {stocks || []}
-        onCreateUser={refreshPage}
-      /> : null}
+    {/* This ternary operator returns the CreateUserModal if showModal = true, else returns null => nothing will be rendered */}
+    {showModal ? <CreateUserModal stocks = {stocks || []} onCreateUser={refreshPage}/> : null}
     </div>
   );
 }

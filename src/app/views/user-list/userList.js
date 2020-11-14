db39f229e6a5d9e619dbcb79d02bcde5a96b0af0
Lasse Nordahl <lasseanordahl@gmail.com>
2020-11-13 20:00:09 -0800

    updated secret

--- src/app/views/user-list/userList.js
+++ src/app/views/user-list/userList.js
@@ -18,7 +18,7 @@ function UserList() {
   // Get the list of users through a GET request to the backend API
   async function getUserList() {
     let users = await axios.get("https://zothacks-2020-workshop.herokuapp.com/user");
-    
+
     // If we get a valid response, set the state object, or print an error.
     if (users.status === 200) {
       setUsers(users.data.data);
--- src/app/views/user-stock/userStock.js
+++ src/app/views/user-stock/userStock.js
@@ -54,7 +54,7 @@ function UserStock() {
   // Performs a GET request to the Finnhub API to get some prices from the stock 
   async function getStockPrices() {
     let finnhubResponse = await axios.get(
-      `https://finnhub.io/api/v1/stock/candle?symbol=${stock.symbol}&resolution=1&from=1572651390&to=1572910590&token=${process.env.REACT_APP_FINNHUB_SECRET}`
+      `https://finnhub.io/api/v1/stock/candle?symbol=${stock.symbol}&resolution=5&from=1588167000&to=1588190000&token=${process.env.REACT_APP_FINNHUB_SECRET}`
     );
 
     // If we have a valid response, we can set the object to represent the prices
